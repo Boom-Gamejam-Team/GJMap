89db55a38406183c93b2e64d8967dcd13607f9b8
hx3357 <hx3357@126.com>
2023-10-04 22:40:53 +0800

    Implemented set spawn point feature

---
+++ Assets/Script/Level/Level.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    //the spawn position of the player in current level 
+    public Vector2 spawnPos;
+}
--- Assets/Script/Map/MapEditor.cs
+++ Assets/Script/Map/MapEditor.cs
@@ -19,6 +19,10 @@ public class MapEditor : Singleton<MapEditor>
     public int gridGroupSize = 3;
     public LayerMask rayLayerMask;
 
+    [Header("Level options")]
+    public bool isAutoFindLevel;
+    public Level currentLevel;
+
     [HideInInspector]
     public List<Grid> gridList;
 
@@ -28,6 +32,7 @@ public class MapEditor : Singleton<MapEditor>
     public Text idTxt;
     public Text posTxt;
     public Dropdown typeDropdown;
+    public Text isSpawnPointTxt;
 
     private Grid currentSelectedGrid;
     private GameObject gridDummyObjInstance;
@@ -39,6 +44,10 @@ public class MapEditor : Singleton<MapEditor>
         gridDummyObjInstance.SetActive(false);
         gridDummySelectedObjInstance = Instantiate(gridDummySelectedObj);
         gridDummySelectedObjInstance.SetActive(false);
+        if (isAutoFindLevel)
+        {
+            currentLevel = FindObjectOfType<Level>();
+        }
     }
 
     private void Start()
@@ -95,23 +104,24 @@ public class MapEditor : Singleton<MapEditor>
 
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, 1000, rayLayerMask) && !EventSystem.current.IsPointerOverGameObject())
-        {
-            Grid castedGrid = hit.collider.GetComponent<Grid>();
-            if (castedGrid == null) return;
-            EnableDummyGrid(castedGrid.transform.position);
-            if (Input.GetMouseButton(0))
+        if (!EventSystem.current.IsPointerOverGameObject())
+            if (Physics.Raycast(ray, out hit, 1000, rayLayerMask))
             {
-                SelectGrid(castedGrid);
+                Grid castedGrid = hit.collider.GetComponent<Grid>();
+                if (castedGrid == null) return;
+                EnableDummyGrid(castedGrid.transform.position);
+                if (Input.GetMouseButton(0))
+                {
+                    SelectGrid(castedGrid);
+                }
             }
-        }
-        else
-        {
-            if (Input.GetMouseButton(0))
+            else
             {
-                UnselectAllGrid();
+                if (Input.GetMouseButton(0))
+                {
+                    UnselectAllGrid();
+                }
             }
-        }
     }
 
     void EnableDummyGrid(Vector3 pos, bool isSelecting = false)
@@ -162,6 +172,7 @@ public class MapEditor : Singleton<MapEditor>
         posTxt.text = "q: " + currentSelectedGrid.hexPos.x + " r: " + currentSelectedGrid.hexPos.y + " s: " +
             (1 - currentSelectedGrid.hexPos.x - currentSelectedGrid.hexPos.y);
         typeDropdown.value = (int)currentSelectedGrid.type;
+        isSpawnPointTxt.text = currentSelectedGrid.hexPos == currentLevel.spawnPos ? "Spawn Point" : "";
     }
 
     public void SubmitGridChange()
@@ -172,7 +183,6 @@ public class MapEditor : Singleton<MapEditor>
             return;
         }
         currentSelectedGrid.ChangeGridType((GridType)typeDropdown.value);
-
     }
     public void DeleteGrid()
     {
@@ -193,5 +203,16 @@ public class MapEditor : Singleton<MapEditor>
         Destroy(currentSelectedGrid.gameObject);
         gridList.Remove(currentSelectedGrid);
         UnselectAllGrid();
+    }
+
+    public void SetSpawnPoint()
+    {
+        if (currentSelectedGrid == null)
+        {
+            Debug.Log("No grid selected");
+            return;
+        }
+        currentLevel.spawnPos = currentSelectedGrid.hexPos;
+        ChangeUIElement();
     }
 }
